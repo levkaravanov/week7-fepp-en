ebef78029da42f36f11ccb98f4a92ddc4f193e06
Lev Karavanov <153912980+levkaravanov@users.noreply.github.com>
2025-10-01 11:39:46 +0300

    Add user routes and integrate with app
    Introduced userRouter with login and signup endpoints and registered it under /api/users in the main app. This sets up the backend for user authentication functionality.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const productRouter = require("./routes/productRouter");
+const userRouter = require("./routes/userRouter");
 const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -14,7 +15,7 @@ connectDB();
 
 // Use the productRouter for all "/products" routes
 app.use("/api/products", productRouter);
-
+app.use("/api/users", userRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
